79721e7ae534be925467be8541e624c04b5b02f3
felicia Meyer <feliciameyer1@student.purdueglobal.edu>
2020-12-14 12:53:38 -0500

    Learning about JSX

--- src/index.js
+++ src/index.js
@@ -5,7 +5,12 @@ import ReactDOM from 'react-dom';
 
 //create a react component
 const App= function() {
-    return <div>Hi there!</div>;
+    const buttonText="click me"
+    return <div>
+        <label class="label" for="name">Enter your name:</label>
+        <input type="text" id="name"/>
+<button style={{backgroundColor:'blue', color:'white'}} >{buttonText}</button>
+        </div>;
 };
 
 
